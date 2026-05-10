96e65f8909ab035d0130175e0ab439a0b1b4e193
Omar Negm <omarnegm2014@gmail.com>
2026-05-10 15:05:51 +0300

    Remove direct loading of tokenizer and model
    Removed direct model loading code for clarity.

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ from transformers import pipeline
 
 #pipe = pipeline("text-classification", model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
 
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased-finetuned-sst-2-english")
-model = AutoModelForSequenceClassification.from_pretrained("distilbert/distilbert-base-uncased-finetuned-sst-2-english")
 
 import os
 os.environ['HF_TOKEN'] = os.getenv("HF_TOKEN")
